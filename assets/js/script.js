f24e2e211063a1b84b6ec944c33059aceed8264e
Shanbashir1 <shanbashir1@hotmail.com>
2022-08-05 10:14:23 +0000

    Added functions on to JavaScript and using comments to define the function of each

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+ /**
+ * Start Game Function and setting variables to 0
+ */
+function startGame() {
+
+}
+
+ /**
+ * This function will show the Question 
+ */
+function showQuestion(question) {
+
+}
+
+/**
+ * This function will give the user the ability to select answers from the arrays of listed answers. 
+ */
+ function selectAnswer(i) {
+
+ }
+
+ /**
+ * This function select the correct and incorrect answers using "correct and wrong elements". 
+ */
+ function setStatusClass(){
+ }
+
+  /**
+ * This function  to clear status of the selected correct and incorrect answers.
+ */
+ function clearStatusClass()
+
+
+
+ 
